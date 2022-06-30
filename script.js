b9c62404daf7594460041db15bf935d35d12b1a4
ann <dorisann.me@gmail.com>
2022-06-30 14:01:29 +0000

    updated the game to 5 rounds and added scores

--- script.js
+++ script.js
@@ -1,19 +1,57 @@
+//helper functions
+let computerScore = 0;
+let playerScore = 0;
+let win = `You Win!`;
+let lose = `You Lose!`;
+let tie = "It's a tie!";
 const options = ["rock", "paper", "scissors"];
 
+// a function that randomly returns rock, paper or scissors
 function computerPlay(options) {
-  return options[Math.floor(Math.random() * options.length)]; // a function that randomly returns rock, paper or scissors
+  return options[Math.floor(Math.random() * options.length)];
 }
 computerPlay(options);
 
-// a function that takes a computer and player input and returns the winner of the round
-function playRound(computerSelection, playerSelection) {
-  if (playerSelection === "rock" && computerSelection === "paper") {
-    return "You Lose! Paper beats rock";
+// a function that takes a computer and player input and returns the winner of the round and increase scores too.
+function game(computerSelection, playerSelection) {
+  if (playerSelection === "rock") {
+    if (computerSelection === "paper") {
+      computerScore++;
+      return lose;
+    } else {
+      playerScore++;
+      return win;
+    }
+  }
+  if (playerSelection === "paper") {
+    if (computerSelection === "scissors") {
+      computerScore++;
+      return lose;
+    } else {
+      playerScore++;
+      return win;
+    }
+  }
+  if (playerSelection === "scissors") {
+    if (computerSelection === "rock") {
+      computerScore++;
+      return lose;
+    } else {
+      playerScore++;
+      return win;
+    }
+  }
+  if (playerSelection === computerSelection) {
+    return tie;
   } else {
-    return "invalid input";
+    return "Invalid Input";
   }
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay(options);
-console.log(playRound(computerSelection, playerSelection));
+for (let i = 0; i < 5; i++) {
+  const playerSelection = prompt("Enter your play!").toLowerCase();
+  const computerSelection = computerPlay(options);
+  console.log(game(computerSelection, playerSelection));
+  console.log(`Your Score: ${playerScore}`);
+  console.log(`Computer Score: ${computerScore}`);
+}
