afbcb5e45cb24e367f876c4fa09d0aafdcb1d6f2
ann <dorisann.me@gmail.com>
2022-07-07 14:42:02 +0000

    displayed the results

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 let computerScore = 0;
 let playerScore = 0;
+let ties = 0;
 const options = ["rock", "paper", "scissors"];
 
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
+    console.log("a button is clicked");
+
     function game() {
       const playerSelection = button.id;
       const computerSelection = computerPlay(options);
-      console.log(playRound(computerSelection, playerSelection));
+      playRound(computerSelection, playerSelection);
 
       function computerPlay(options) {
         return options[Math.floor(Math.random() * options.length)];
@@ -18,14 +21,17 @@ buttons.forEach((button) => {
       //results
       let gameResults = document.querySelector(".results");
       gameResults.innerHTML = `
-        <p>Computer score: ${computerScore}</p>
-        <p>Player score: ${playerScore}</p>`;
+        <p>Computer Selection: ${computerSelection}</p>
+        <p>Player Selection: ${playerSelection}</p>
+        <p>Computer wins: ${computerScore}</p>
+        <p>Player wins: ${playerScore}</p>
+        <p>Ties: ${ties}</p>`;
     }
 
     function playRound(computerSelection, playerSelection) {
-      let win = `YOU WIN ${playerSelection} beats ${computerSelection}`;
-      let lose = `YOU LOSE! ${computerSelection} beats ${playerSelection}`;
-      let tie = "IT'S A TIE! YOU BOTH CHOSE THE SAME THING";
+      const win = `YOU WIN ${playerSelection} beats ${computerSelection}`;
+      const lose = `YOU LOSE! ${computerSelection} beats ${playerSelection}`;
+      const tie = "IT'S A TIE! YOU BOTH CHOSE THE SAME THING";
 
       if (playerSelection === "rock") {
         if (computerSelection === "paper") {
@@ -59,7 +65,7 @@ buttons.forEach((button) => {
         playerSelection === computerSelection &&
         computerSelection === playerSelection
       ) {
-        return tie;
+        ++ties;
       } else {
         return "Invalid Input";
       }
